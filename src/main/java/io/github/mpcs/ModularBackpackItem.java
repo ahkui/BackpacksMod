ffc7e7cbd4bedaf9abb1daaadc57b60f6c58f5ab
Mpcs <kuba.mpcs@gmail.com>
2019-05-25 13:51:28 +0200

    Created modular backpack item with GUI

---
+++ src/main/java/io/github/mpcs/ModularBackpackItem.java
@@ -0,0 +1,46 @@
+package io.github.mpcs;
+
+import io.github.mpcs.init.Items;
+import net.fabricmc.fabric.api.container.ContainerProviderRegistry;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.Inventories;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.DefaultedList;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.world.World;
+
+public class ModularBackpackItem extends Item {
+    public ModularBackpackItem(Settings item$Settings_1) {
+        super(item$Settings_1);
+    }
+
+    @Override
+    public TypedActionResult<ItemStack> use(World world_1, PlayerEntity playerEntity_1, Hand hand_1) {
+        if (!world_1.isClient)
+            ContainerProviderRegistry.INSTANCE.openContainer(Items.MODULAR_BACKPACK_CONTAINER, playerEntity_1, buf -> {
+
+            });
+        return new TypedActionResult(ActionResult.PASS, playerEntity_1.getStackInHand(hand_1));
+    }
+
+    public static void setInventory(ItemStack backpack, DefaultedList<ItemStack> list) {
+        CompoundTag nbt = new CompoundTag();
+        Inventories.toTag(nbt, list);
+        backpack.setTag(nbt);
+    }
+
+    public static void getInventory(ItemStack backpack, DefaultedList<ItemStack> defaultedList) {
+        CompoundTag nbt;
+        if(backpack.hasTag()) {
+            nbt = backpack.getTag();
+        } else {
+            nbt = new CompoundTag();
+        }
+        Inventories.fromTag(nbt, defaultedList);
+        backpack.setTag(nbt);
+    }
+}
--- src/main/java/io/github/mpcs/init/Items.java
+++ src/main/java/io/github/mpcs/init/Items.java
@@ -1,6 +1,7 @@
 package io.github.mpcs.init;
 
 import io.github.mpcs.BackpackItem;
+import io.github.mpcs.ModularBackpackItem;
 import io.github.mpcs.ModularBackpacksMod;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
@@ -9,10 +10,13 @@ import net.minecraft.util.registry.Registry;
 
 public class Items {
     public static final Identifier SMALL_BACKPACK_CONTAINTER = new Identifier(ModularBackpacksMod.modid, "small_backpack");
+    public static final Identifier MODULAR_BACKPACK_CONTAINER = new Identifier(ModularBackpacksMod.modid, "modularbackpack");
 
     public static Item BACKPACK = new BackpackItem(new Item.Settings().itemGroup(ItemGroup.TOOLS).stackSize(1));
+    public static Item MODULAR_BACKPACK = new ModularBackpackItem(new Item.Settings().itemGroup(ItemGroup.TOOLS).stackSize(1));
 
     public static void init() {
         Registry.register(Registry.ITEM, new Identifier(ModularBackpacksMod.modid, "backpack"), BACKPACK);
+        Registry.register(Registry.ITEM, new Identifier(ModularBackpacksMod.modid, "modularbackpack"), MODULAR_BACKPACK);
     }
 }
